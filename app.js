98299c0e51b54cad8c8fdaffec93b4fe9a250aba
Auburn9698 <atkingn67@yahoo.com>
2020-12-29 13:21:12 -0500

    1st update
    1st update small

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-d3.json("data/samples.json").then((data) => 
+// metadata: [id, ethnicity, gender, age, location, bbtype, wfreq]
+// "metadata":[{"id": 940, "ethnicity": "Caucasian", "gender": "F", "age": 24.0, "location": "Beaufort/NC", "bbtype": "I", "wfreq": 2.0},
+// samples: [id (values), otu_ids (lists), sample_values (lists), otu_labels (lists)]
+
+var select = document.getElementById("selDataset");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+d3.json("data/samples.json").then((data) =>  {
+
+});
